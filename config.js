3be3742c2753499c49082f7c01ec70b03180a3ed
ebbflowgo <christopher.dolinski@gmail.com>
2013-12-15 14:33:27 -0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+var config = {}
+config.twitter = {};
+
+// https://dev.twitter.com/apps - sign up for an app to fill these in
+config.twitter.consumer_key = "Put consumer key here";
+config.twitter.consumer_secret = "Put consumer secret here";
+config.twitter.access_token = "Put access_token here";
+config.twitter.access_token_secret = "Put access token secret here";
+
+module.exports = config;
